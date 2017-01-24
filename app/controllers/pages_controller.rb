1ccc5cda48ffcb855a53eeb42c0ddd14b80cdee3
Reggie Cooper <reginvld@gmail.com>
2017-01-24 23:27:13 +0000

    Added blog, contact pages. Refactored with sass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
     
     def about
     end
+    
+    def blog
+    end
+    
+    def contact
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'blog', to: 'pages#blog'
+  get 'contact', to: 'pages#contact'
 end
